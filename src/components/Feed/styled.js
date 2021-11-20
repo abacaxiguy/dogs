82630b138a3ca12d415262e133f3eab3a0c2b85f
joao <jlucasoc@hotmail.com>
2021-11-19 22:17:48 -0300

    Creating Feed component 🚀

---
+++ src/components/Feed/styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Head = styled.header`
+  box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.1);
+  position: fixed;
+  width: 100%;
+  z-index: 100;
+  background: white;
+  top: 0;
+`;
--- src/pages/Photos/index.js
+++ src/pages/Photos/index.js
@@ -1,29 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 import { Container } from '../../styles/GlobalStyles';
-import axios from '../../services/axios';
+import Feed from '../../components/Feed';
 
 export default function Photos() {
-  const [photos, setPhotos] = useState([]);
-
-  useEffect(() => {
-    async function getData() {
-      const response = await axios.get('/photos');
-      setPhotos(response.data);
-    }
-
-    getData();
-  }, []);
-
   return (
     <Container>
-      {photos.map((photo) => (
-        <div key={String(photo.id)}>
-          <span>{photo.title}</span>
-          <img src={photo.url} alt={photo.title} />
-        </div>
-      ))}
-      Photos
+      <Feed />
     </Container>
   );
 }
